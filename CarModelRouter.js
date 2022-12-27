4abb8f7d29d7315e9f1586c5657416710045a422
Vanhue265 <vanhue2652k2@gmail.com>
2022-12-28 03:41:10 +0700

    first commit

---
+++ CarModelRouter.js
@@ -0,0 +1,88 @@
+var expesss = require('express')
+const { ObjectId } = require('mongodb')
+const { insertProduct, allProduct, deleteProduct, updateProduct, findProductById,searchProductByName } = require('./CarModelService')
+var app = expesss()
+
+var MongoClient = require('mongodb').MongoClient
+var url = 'mongodb://0.0.0.0:27017'
+
+app.set('view engine','hbs')
+app.use(expesss.urlencoded({extended:true}))
+
+app.post('/search',async (req,res)=>{
+    const search = req.body.search
+    const results = await searchProductByName(search)
+    console.log(results)
+    res.render('allProduct',{'results':results})
+})
+
+app.post('/edit',async (req,res)=>{
+    const id = req.body.id
+    const name = req.body.txtName
+    const price = req.body.txtPrice
+    const type = req.body.txtType
+    const picture = req.body.txtPic
+    await updateProduct(id, name, price, type, picture)
+    res.redirect('/allProduct')
+})
+
+app.get('/edit',async (req,res)=>{
+    const id = req.query.id
+    const productToEdit = await findProductById(id)
+    res.render('edit',{product:productToEdit})
+})
+
+app.get('/delete',async (req,res)=>{
+    const id = req.query.id
+    await deleteProduct(id)
+    res.redirect('/all')
+})
+
+app.get('/allProduct',async (req,res)=>{
+    const results = await allProduct()
+    res.render('allProduct',{'results':results})
+})
+
+app.post('/new',async (req,res)=>{
+    const name = req.body.txtName
+    const price = req.body.txtPrice
+    const type = req.body.txtType
+    const picUrl = req.body.txtPic
+    const newProduct = {
+        name :name,
+        price: Number.parseFloat(price),
+        type: type,
+        picture: picUrl
+    }
+    let id = await insertProduct(newProduct)
+    console.log(id)
+    res.render('home')
+
+})
+
+app.get('/all',async (req,res)=>{
+    const results = await allProduct()
+    res.render('allProduct',{results:results})
+})
+
+app.get('/new',(req,res)=>{
+    res.render('newProduct')
+})
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
+console.log("Server is running!")
+
+
+
+
+
+
+
+
+
+
---
+++ CarModelService.js
@@ -0,0 +1,45 @@
+var MongoClient = require('mongodb').MongoClient
+var url = 'mongodb://0.0.0.0:27017'
+const { Int32, ObjectId } = require('bson')
+
+async function insertProduct(newProduct) {
+    let client = await MongoClient.connect(url)
+    let db = client.db("Asm2")
+    let id = await db.collection("products").insertOne(newProduct)
+    return id
+}
+
+async function allProduct() {
+    let client = await MongoClient.connect(url)
+    let db = client.db("Asm2")
+    let results = await db.collection("products").find().toArray()
+    return results
+}
+
+async function deleteProduct(id) {
+    let client = await MongoClient.connect(url)
+    let db = client.db("Asm2")
+    await db.collection("products").deleteOne({ _id: ObjectId(id) })
+}
+
+async function updateProduct(id, name, price, type, picture) {
+    let client = await MongoClient.connect(url)
+    let db = client.db("Asm2")
+    await db.collection("products").updateOne({ _id: ObjectId(id) },
+        { $set: { "name": name, "price": price, "type": type, "picture": picture } })
+}
+
+async function findProductById(id) {
+    let client = await MongoClient.connect(url)
+    let db = client.db("Asm2")
+    const productToEdit = await db.collection("products").findOne({ _id: ObjectId(id) })
+    return productToEdit
+}
+
+async function searchProductByName(name){
+    let client = await MongoClient.connect(url)
+    let db = client.db("Asm2")
+    const results = await db.collection("products").find({ name: new RegExp(name,'i') }).toArray()
+    return results
+}
+module.exports = {insertProduct, allProduct, deleteProduct, updateProduct, findProductById, searchProductByName}
